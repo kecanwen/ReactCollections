a6c1c15c73a737a01ff75eef88c22469b3853b53
kecanwen <kcw199611252815@163.com>
2024-02-01 14:25:06 +0800

    kk_component

---
+++ resourse/fiber.js
@@ -0,0 +1,12 @@
+// 一个简化的fiber节点
+const fiberNode = {
+    type: null,
+    key: null,
+    props: null,
+    stateNode: null,
+    child: null,
+    sibling: null,
+    return: null,
+    alternate: null,
+    
+}
