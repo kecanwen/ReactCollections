d36a5cd8c3a589f12b31d909ab41367a307f20d1
kecanwen <kcw199611252815@163.com>
2024-04-18 11:43:53 +0800

    feat

--- kk_components/src/App.js
+++ kk_components/src/App.js
@@ -1,13 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
 import Button, { ButtonType, ButtonSize } from './components/Button/index.tsx';
+import { createContext, useEffect, useState } from 'react';
+
 
 function App() {
+  const [count ,setCount] = useState('');
+  const [range,setRange] = useState('');
+  const [ arr, setArr] = useState([
+   
+  ]);
+  useEffect(() =>{
+
+  },[]);
+  const createContent = ()=>{
+    let arr = [];
+    for(let i=0;i<count;i++){
+      let height = Math.floor(Math.random()*range)
+      arr.push(<li key={i} style={{
+        height
+      }}>{height}</li>)
+    }
+    setArr(arr); 
+  }
   return (
     <div className="App">
-      <Button>按钮</Button>
-      <Button type={ButtonType.Primary} size={ButtonSize.Large}>按钮</Button>
-      <Button type={ButtonType.Link} href="www.baidu.com">按钮</Button>
+      <div className='box'>
+        <ul className=''>
+          {
+            arr.map((item, index) => {
+              return item
+            })
+          }
+        </ul> 
+      </div>
+      <label>范围</label><input onInput={(e)=>{setRange(e.target.value)}} value={range}></input> 
+        <label>个数</label><input onInput={(e)=>{setCount(e.target.value)}} value={count}></input> 
+        <button onClick={createContent} style={{}}> 创建</button> 
     </div>
   );
 }
