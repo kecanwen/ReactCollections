729f08741a2828e405720a17f4484b5185efeb8b
kecanwen <kcw199611252815@163.com>
2024-05-21 19:30:04 +0800

    refreshDepsAction

--- Hooks/ahooks/useRequest/types.ts
+++ Hooks/ahooks/useRequest/types.ts
@@ -2,8 +2,59 @@ export type Plugin<TData, TParams extends any[]> = {
   ()
 }
 // Options, Plugin, Service
-export type Options = {
+export interface Options<TData, TParams extends any[]> {
+  manual?: boolean;
 
+  onBefore?: (params: TParams) => void;
+  onSuccess?: (data: TData, params: TParams) => void;
+  onError?: (e: Error, params: TParams) => void;
+  // formatResult?: (res: any) => TData;
+  onFinally?: (params: TParams, data?: TData, e?: Error) => void;
+
+  defaultParams?: TParams;
+
+  // refreshDeps
+  refreshDeps?: DependencyList;
+  refreshDepsAction?: () => void;
+
+  // loading delay
+  loadingDelay?: number;
+
+  // polling
+  pollingInterval?: number;
+  pollingWhenHidden?: boolean;
+  pollingErrorRetryCount?: number;
+
+  // refresh on window focus
+  refreshOnWindowFocus?: boolean;
+  focusTimespan?: number;
+
+  // debounce
+  debounceWait?: number;
+  debounceLeading?: boolean;
+  debounceTrailing?: boolean;
+  debounceMaxWait?: number;
+
+  // throttle
+  throttleWait?: number;
+  throttleLeading?: boolean;
+  throttleTrailing?: boolean;
+
+  // cache
+  cacheKey?: string;
+  cacheTime?: number;
+  staleTime?: number;
+  setCache?: (data: CachedData<TData, TParams>) => void;
+  getCache?: (params: TParams) => CachedData<TData, TParams> | undefined;
+
+  // retry
+  retryCount?: number;
+  retryInterval?: number;
+
+  // ready
+  ready?: boolean;
+
+  // [key: string]: any;
 }
 
 export type Service = {
